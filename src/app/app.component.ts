72976bbf986ccfaef6a06750587c20ab68eb90f4
SkyZH <iSkyZH@gmail.com>
2016-06-28 19:59:17 +0800

    add i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,41 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { CollapseDirective } from 'ng2-bootstrap/ng2-bootstrap';
+import { TranslateService, TranslatePipe } from 'ng2-translate/ng2-translate';
 
 import { ApiService } from './shared';
 
 import '../style/app.scss';
 
-/*
- * App Component
- * Top Level Component
- */
 @Component({
   selector: 'my-app',
   providers: [ApiService],
+  pipes: [TranslatePipe],
   directives: [...ROUTER_DIRECTIVES, CollapseDirective],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   url = 'https://github.com/WindHub/WindOJ';
-  public isNavbarCollapsed: boolean = false;
 
-  constructor(private api: ApiService) {
+  navs = {
+    main: [
+      { name: 'home', link: '' },
+      { name: 'problem', link: 'problem' },
+      { name: 'contest', link: 'contest' },
+      { name: 'status', link: 'status' },
+      { name: 'discuss', link: 'discuss' },
+      { name: 'rank', link: 'rank' }
+    ]
+  };
+
+  isNavbarCollapsed: boolean = false;
+
+  constructor(private api: ApiService, private translate: TranslateService) {
+    let userLang = navigator.language.split('-')[0];
+    userLang = /(fr|en)/gi.test(userLang) ? userLang : 'en';
+    translate.setDefaultLang('en');
+    translate.use(userLang);
   }
 
 
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { enableProdMode } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { HTTP_PROVIDERS } from '@angular/http';
+import { Http, HTTP_PROVIDERS } from '@angular/http';
+import { TranslateService, TranslateLoader, TranslateStaticLoader } from 'ng2-translate/ng2-translate';
 
 import { AppComponent } from './app/app.component';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 
 const ENV_PROVIDERS = [];
-// depending on the env mode, enable prod mode or add debugging modules
+
 if (process.env.ENV === 'build') {
   enableProdMode();
 } else {
@@ -16,10 +17,15 @@ if (process.env.ENV === 'build') {
 }
 
 bootstrap(AppComponent, [
-    // These are dependencies of our App
     ...HTTP_PROVIDERS,
     ...APP_ROUTER_PROVIDERS,
     ...ENV_PROVIDERS,
-    { provide: LocationStrategy, useClass: HashLocationStrategy } // use #/ routes, remove this for HTML5 mode
+    { provide: LocationStrategy, useClass: HashLocationStrategy},
+    {
+      provide: TranslateLoader,
+      useFactory: (http: Http) => new TranslateStaticLoader(http, 'i18n', '.json'),
+      deps: [Http]
+    },
+    TranslateService
   ])
   .catch(err => console.error(err));
