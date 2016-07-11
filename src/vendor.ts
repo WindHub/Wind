f356e1587b42aed070d318881f9e75c037c2af7c
SkyZH <iSkyZH@gmail.com>
2016-07-11 22:22:14 +0800

    add travis and fix moment

--- src/vendor.ts
+++ src/vendor.ts
@@ -9,6 +9,8 @@ import 'rxjs';
 
 import 'ng2-translate/ng2-translate';
 import 'ng2-bootstrap/ng2-bootstrap';
-import * as moment from 'angular2-moment';
+
+import * as moment from 'moment';
+import 'angular2-moment';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
