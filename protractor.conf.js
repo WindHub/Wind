8927cd362a2bc97eeef983b4c159e9073428ecaa
SkyZH <iSkyZH@gmail.com>
2016-07-15 09:35:10 +0800

    modify protractor config

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,4 @@
 exports.config = {
-  baseUrl: 'http://localhost:8080/',
-
   specs: [
     'src/**/*.e2e-spec.js'
   ],
@@ -39,5 +37,35 @@ exports.config = {
    * `rootEl`
    *
    */
-  useAllAngular2AppRoots: true
+  useAllAngular2AppRoots: true,
+
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  // restartBrowserBetweenTests: true,
+
+  multiCapabilities: [{
+    browserName: 'firefox',
+    version: '32',
+    platform: 'OS X 10.10',
+    name: "firefox-tests",
+    shardTestFiles: true,
+    maxInstances: 5
+  }, {
+    browserName: 'chrome',
+    version: '41',
+    platform: 'Windows 7',
+    name: "chrome-tests",
+    shardTestFiles: true,
+    maxInstances: 5
+  }],
+
+  onComplete: function() {
+    var printSessionId = function(jobName){
+      browser.getSession().then(function(session) {
+        console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+      });
+    };
+    printSessionId("wind-ng");
+  }
 };
